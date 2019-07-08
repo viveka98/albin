02c1c237ad474207a7b5654da5691dbe10469881
viveka98 <52188971+viveka98@users.noreply.github.com>
2019-07-08 19:45:43 +0530

    Create eve.c

---
+++ eve.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+int num,m,n;
+scanf("%d %d",&m,&n);
+for (num = m+1; num <=n-1; num++)
+{
+ if (num % 2 == 0)
+ printf ("%d ", num);
+}
+return 0;                
+}
